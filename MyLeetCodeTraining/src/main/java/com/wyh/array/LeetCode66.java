e9258cb55d18a1462623734aa666f265561f8d55
wuyuhao <727162553@qq.com>
2022-04-19 13:13:38 +0800

    十道题

---
+++ MyLeetCodeTraining/src/main/java/com/wyh/array/LeetCode66.java
@@ -0,0 +1,21 @@
+package com.wyh.array;
+
+/**
+ * @Author wu yuhao
+ * @Date 2022/4/18
+ * @Description
+ */
+public class LeetCode66 {
+    public int[] plusOne(int[] digits) {
+        int length = digits.length;
+        for (int i = length - 1; i >= 0; i--) {
+            digits[i] = (digits[i] + 1) % 10;
+            if (digits[i] != 0) {
+                return digits;
+            }
+        }
+        int[] result = new int[length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
--- MyLeetCodeTraining/src/main/java/com/wyh/listNode/ListNode.java
+++ MyLeetCodeTraining/src/main/java/com/wyh/listNode/ListNode.java
@@ -5,11 +5,23 @@ package com.wyh.listNode;
  * @Date 2022/2/1
  * @Description
  */
+
+/**
+ * Definition for singly-linked list.
+ */
 public class ListNode {
     int val;
     ListNode next;
-    ListNode(int x) {
-        val = x;
-        next = null;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
