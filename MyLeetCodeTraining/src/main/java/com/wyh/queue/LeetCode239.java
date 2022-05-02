bdd3629dd4bf3a7490c72b392781c7fee2f49bc3
wuyuhao <727162553@qq.com>
2022-05-03 00:25:36 +0800

    Add BinarSearch

--- MyLeetCodeTraining/src/main/java/com/wyh/queue/LeetCode239.java
+++ MyLeetCodeTraining/src/main/java/com/wyh/queue/LeetCode239.java
@@ -1,5 +1,6 @@
 package com.wyh.queue;
 
+import javax.swing.*;
 import java.util.Deque;
 import java.util.LinkedList;
 
@@ -12,17 +13,19 @@ public class LeetCode239 {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
         Deque<Integer> deque = new LinkedList<>();
+        // 2, 1  ----3
+        // 3
         for (int i = 0; i < k; i++) {
-            while (!deque.isEmpty() && nums[i] > deque.peekLast()) {
+            while (!deque.isEmpty() && nums[i] > nums[deque.peekLast()]) {
                 deque.pollLast();
             }
             deque.offerLast(i);
         }
         int[] ans = new int[n - k + 1];
-        ans[0] = deque.peekFirst();
+        ans[0] = nums[deque.peekFirst()];
         for (int i = k; i < n; i++) {
-            while (!deque.isEmpty() && nums[i] > deque.peekLast()) {
-                deque.peekLast();
+            while (!deque.isEmpty() && nums[i] > nums[deque.peekLast()]) {
+                deque.pollLast();
             }
             deque.offerLast(i);
             while (deque.peekFirst() <= i - k) {
